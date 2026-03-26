9b93058a5a35a4dbedc8a9f670854c159f807fe3
julianlamaravilla <julianrestrepo012@gmail.com>
2026-03-26 16:32:25 -0500

    second version with JOptionPane

--- src/NameManage.java
+++ src/NameManage.java
@@ -1,34 +1,30 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class NameManage {
 
     public static void main(String[] args){
 
-        Scanner scanner = new Scanner(System.in);
-
         String firstName = null;
         String secondName = null;
         String thirdName = null;
 
         try {
-            System.out.println("Please Enter first name \b: ");
-            firstName = scanner.next();
 
+            firstName = JOptionPane.showInputDialog(null, "Please Enter first name \b: ");
             String newFirst = firstName.substring(1,2).concat(".") + firstName.substring(firstName.length() - 2);
 
-            System.out.println("Please Enter second name \b: ");
-            secondName = scanner.next();
-
+            secondName = JOptionPane.showInputDialog(null, "Please Enter second name \b: ");
             String newSecond = secondName.substring(1,2).concat(".") + secondName.substring(secondName.length() - 2);
 
-            System.out.println("Please Enter third name \b: ");
-            thirdName = scanner.next();
 
+            thirdName = JOptionPane.showInputDialog(null , "Please Enter third name \b: ");
             String newThird = thirdName.substring(1,2).concat(".") + thirdName.substring(thirdName.length() - 2);
 
-            System.out.println(newFirst.concat("_").concat(newSecond).concat("_").concat(newThird));
+
+            JOptionPane.showMessageDialog(null, newFirst.concat("_").concat(newSecond).concat("_").concat(newThird).toUpperCase());
         } catch (Exception e){
-            System.out.println("Please, enter right numbers");
+            JOptionPane.showMessageDialog(null ,  "Please, enter right numbers");
             main(args);
             System.exit(0);
         }
